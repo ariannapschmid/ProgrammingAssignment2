8704d55d274a5217e0f9ee2adaf24383bd5b1e79
Ariann Schmid <ariannaschmid@ufl.edu>
2020-05-03 14:38:20 -0400

    Assisnment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Below, the object myInverse is being initialized to NULL 
+  # x is already initialized as a function argument
+  myInverse <- NULL
+  
+  #Below, we are assigning the input to x as well as 
+  # assigning the object myInverse to NULL. This will reset 
+  # any previous value of myInverse that had been cached
+  set <- function (y){
+    x <<- y
+    myInverse <<- NULL
+  }
+  #Below, we retrieve ("get") our matrix x.
+  get <- function() x
+  
+  #Below, we set the inverse
+  setinverse <- function(inverse) myInverse <<- inverse
+  
+  #Below, we retrieve ("get") the inverse
+  getinverse <- function() myInverse
+  
+  # Below, we assign names to each of the functions above, and place them in
+  # a list to be returned to the parent environment
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+  # end of makeCacheMatrix
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Below, we try to retrieve the inverted matrix by calling getInverse()
+  # Then, in the if statement, we check to see if the inverse had already
+  # been cached previously (this follows if !is.null(myInverse) is TRUE). If
+  # TRUE, we return the inverted matrix. 
+  myInverse <- x$getinverse()
+  if(!is.null(myInverse)) {
+    message("getting cached data")
+    return(myInverse)
+  }
+  #If FALSE, we retrieve the matrix from the input, calculate the inverse,
+  # then call setInverse to set the calculated inverse to the inputed matrix object 
+  # then we return the inverted matrix
+  data <- x$get()
+  myInverse <- solve(data, ...)
+  x$setinverse(myInverse)
+  myInverse
+  
 }
+
